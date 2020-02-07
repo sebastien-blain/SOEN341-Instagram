7962656e49e78630788f9af713139cc1718da284
Sebastien <sbnadeau@accedian.com>
2020-02-06 22:43:38 -0500

    FEAT-#26: Updated routes with real created ones

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -2,7 +2,6 @@ from .users import UserApi, PictureApi, LoginApi, RegisterApi
 
 
 def initialize_routes(api):
-    api.add_resource(UserApi, '/api/user/<string:username>')
-    api.add_resource(PictureApi, '/api/picture/<string:name>')
     api.add_resource(LoginApi, '/api/login')
-    api.add_resource(RegisterApi, '/api/register/<string:name>')
+    api.add_resource(RegisterApi, '/api/register')
+    api.add_resource(UserApi, '/api/user/<string:username>')
