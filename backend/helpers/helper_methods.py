e5064d07720b5769d79332a07c803059a7056a5a
Sebastien <sbnadeau@accedian.com>
2020-02-06 22:41:59 -0500

    FEAT-#26: new helper verify field email

--- backend/helpers/helper_methods.py
+++ backend/helpers/helper_methods.py
@@ -1,5 +1,16 @@
-def fields_are_in(dict, fields):
+import re
+
+
+def fields_are_in(dicts, fields):
     for i in fields:
-        if i not in dict:
+        if i not in dicts:
             return False
+    return True
+
+
+def is_email(email):
+    r = re.compile(
+        r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    if not r.match(email):
+        return False
     return True
