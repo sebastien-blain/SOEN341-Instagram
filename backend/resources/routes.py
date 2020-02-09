4fe5e9722a1ad73b757c74f790e756999548dbc0
Sebastien <43256461+Sebastien0730@users.noreply.github.com>
2020-02-09 14:31:21 -0500

    TASK-#49: Changed route info

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -3,7 +3,7 @@ from .pictures import PostPictureAPI
 
 
 def initialize_routes(api):
-    api.add_resource(LoginApi, '/api/login')
-    api.add_resource(PostPictureAPI, '/api/post')
-    api.add_resource(FollowUserApi, '/api/follow')
-    api.add_resource(UserApi, '/api/user/<string:username>')
+    api.add_resource(LoginApi, '/login')
+    api.add_resource(PostPictureAPI, '/post')
+    api.add_resource(FollowUserApi, '/follow')
+    api.add_resource(UserApi, '/user/<string:username>')
