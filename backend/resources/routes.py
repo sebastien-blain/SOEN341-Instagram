1d9010e77e5bd8a3975630310d02ecfe04ffab40
Sebastien <43256461+Sebastien0730@users.noreply.github.com>
2020-02-09 20:26:49 -0500

    Error in last commit

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -2,7 +2,7 @@ from .users import UserApi, LoginApi, DefaultPage
 
 
 def initialize_routes(api):
-    api.add_ressource(DefaultPage, '/')
+    api.add_resource(DefaultPage, '/')
     api.add_resource(LoginApi, '/login')
     api.add_resource(PostPictureAPI, '/post')
     api.add_resource(FollowUserApi, '/follow')
