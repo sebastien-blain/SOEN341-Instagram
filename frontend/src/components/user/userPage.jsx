d434cfd47f9e14b53119e652333bbc5c3447d22d
xujulien99 <60070633+xujulien99@users.noreply.github.com>
2020-02-13 20:05:09 -0500

    Task-44: Updated GUI of userPage
    Commented "this.state.isUser" in order to show buttons because isUser is always false (properties of isUser are not defined yet).  Added unfollow and message button, counts of posts, followers, following (all set to 0 because undefined). Potential features also added like bio, friends who follow same user and image section.

--- frontend/src/components/user/userPage.jsx
+++ frontend/src/components/user/userPage.jsx
@@ -3,6 +3,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import Message from '@material-ui/icons/Message';
+import Close from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,13 +16,16 @@ const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
   },
+  table: {
+    margin: theme.spacing(1),
+  },
 }));
 
 export default class UserPage extends Component {
   constructor(props) {
     super(props);
     // Don't call this.setState() here!
-    this.state = { classes: useStyles, notFollowing: this.props.notFollowing };
+    this.state = { classes: useStyles, notFollowing: this.props.notFollowing, isUser: this.props.isUser};
   }
 
   follow = () => {
@@ -49,24 +54,72 @@ export default class UserPage extends Component {
       console.log(e)
     })
   }
-
+  
   render() {
     return (
-      <div>
-      <Typography variant="h6" noWrap>
-        {this.props.user}
-      </Typography>
-      <Button
-      variant="contained"
-      color="primary"
-      className={this.state.classes.button}
-      startIcon={<PersonAddIcon />}
-      onClick={this.follow}
-      disabled={this.state.notFollowing}
-      >
-        Follow
-      </Button>
-      </div>
+          <div>
+             <Typography variant="h4" noWrap>
+              <p><strong>{this.props.user}</strong></p>
+            </Typography>
+            <Typography variant="body1" align="left" paragraph="true">
+              <table col width="500">
+                <tr>
+                  <td>{this.follow.length}</td>
+                  <td>{this.follow.length}</td>
+                  <td>{this.follow.length}</td>
+                </tr>
+                <tr>
+                  <td>Posts</td>
+                  <td>Followers</td>
+                  <td>Following</td>
+                </tr>
+              </table>
+            </Typography>
+            <Typography variant="caption" noWrap paragraph="true">
+            <textarea id="bio" cols="60" disabled maxlength="200">
+            *Profile bio* This is a profile bio 
+            </textarea>
+            </Typography>
+            <p>Followed by .... </p>
+            <div>
+              {//this.state.isUser &&
+                <div>
+                <Button
+                variant="contained"
+                color="primary"
+                className={this.state.classes.button}
+                startIcon={<PersonAddIcon />}
+                onClick={this.follow}
+                disabled={!this.state.notFollowing}
+                >
+                  Follow
+                </Button>
+
+                <Button
+                variant="contained"
+                color="secondary"
+                className={this.state.classes.button}
+                startIcon={<Close />}
+                onClick={this.unfollow}
+                disabled={this.state.notFollowing}
+                >
+                  Unfollow
+                </Button>
+
+                <Button
+                variant="contained"
+                color="default"
+                className={this.state.classes.button}
+                startIcon={<Message />}
+                onClick={this.message}
+                >
+                  Message
+                </Button>
+                </div>}
+            </div>
+            <hr />
+            <p>*IMAGE SECTION</p>
+          </div>
       );
   }
 }
