77c86a0e952925a8502574fe4e233021f752b597
Sebastien <sbnadeau@accedian.com>
2020-02-14 18:35:51 -0500

    TASK-#90: Added update bio api route

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -1,4 +1,4 @@
-from .users import LoginApi, DefaultPage, SearchUserAPI, FollowUserApi, FeedAPI, UserInfoAPI, UnfollowUserApi
+from .users import LoginApi, DefaultPage, SearchUserAPI, FollowUserApi, FeedAPI, UserInfoAPI, UnfollowUserApi, updateBioAPI
 from .pictures import PostPictureAPI
 
 
@@ -11,3 +11,4 @@ def initialize_routes(api):
     api.add_resource(UnfollowUserApi, '/unfollow')
     api.add_resource(SearchUserAPI, '/search')
     api.add_resource(UserInfoAPI, '/user/<username>')
+    api.add_resource(updateBioAPI, '/user/update/bio')
