ed0b26e08d734365326d1bf7152474420b475f23
Sebastien <sebastienbn8@gmail.com>
2020-03-23 13:45:00 -0400

    TASK-#132: test main page status

--- backend/helpers/helper_methods.py
+++ backend/helpers/helper_methods.py
@@ -1,4 +1,6 @@
 def fields_are_in(dicts, fields):
+    #print(dicts)
+    #print(fields)
     for i in fields:
         if i not in dicts:
             return False
@@ -6,7 +8,7 @@ def fields_are_in(dicts, fields):
 
 
 def is_empy_or_none(dicts):
-    print(dicts)
+    #print(dicts)
     for i in dicts:
         if dicts[i] is None or dicts[i] == '':
             return True
--- backend/tests/test_test.py
+++ backend/tests/test_test.py
@@ -1,20 +1,44 @@
 import unittest
+import json
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+from flask_cors import CORS, cross_origin
+
+import sys
+sys.path.append('../')
+from database.db import initialize_db
+from resources.routes import initialize_routes
+
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def setUp(self):
+        app = Flask(__name__)
+        CORS(app)
+        app.config['JWT_SECRET_KEY'] = 'idbfiw724yr4fn3y5gt754tbhi54bth45b'
+        api = Api(app)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        bcrypt = Bcrypt(app)
+        jwt = JWTManager(app)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        app.config['MONGODB_SETTINGS'] = {
+            'host': 'mongodb://localhost/mypanda'
+        }
+
+        initialize_db(app)
+        initialize_routes(api)
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEqual(app.debug, False)
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
 
 if __name__ == '__main__':
     unittest.main()
